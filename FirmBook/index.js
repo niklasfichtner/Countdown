a83b78aa54e3b4509e18d8687b3b43532ba49946
Niklas Fichtner <niklasfichtner@web.de>
2018-10-22 14:12:09 +0200

    firebase-add eingebaut

--- FirmBook/index.js
+++ FirmBook/index.js
@@ -4,19 +4,21 @@ $(document).ready(function(){
         $('ul').toggleClass('active')
     })
 })
-//firebase-add 
+//firebase-add
 const firestore = firebase.firestore();
-  const settings = {/* your settings... */ timestampsInSnapshots: true};
+  const settings = { timestampsInSnapshots: true};
   firestore.settings(settings);
 
 // Initialize Firebase
 var db = firebase.firestore();
 function storeData(){
     // Add a new document in collection "cities"
-db.collection("cities").doc("LA").set({
-    name: "Los Angeles",
-    state: "CA",
-    country: "USA"
+db.collection("employee").doc(document.getElementById("employeeName").value).set({
+    name: document.getElementById("employeeName").value,
+    Stellenbezeichnung: document.getElementById("employeeTitel").value,
+    Phone: document.getElementById("employeePhone").value,
+    mail: document.getElementById("employeeMail").value,
+    Standort: document.getElementById("employeeLocation").value
 })
 .then(function() {
     console.log("Document successfully written!");
