16082f49262b75e0b195f7b473890c60451a8241
Niklas Bäuerle <niklas.baeuerle@gmx.de>
2018-10-24 11:19:34 +0200

    eingerückt

--- FirmBook/index.js
+++ FirmBook/index.js
@@ -16,30 +16,30 @@ var config = {
 firebase.initializeApp(config);
 
 const firestore = firebase.firestore();
-  const settings = {timestampsInSnapshots: true};
-  firestore.settings(settings);
+const settings = {timestampsInSnapshots: true};
+firestore.settings(settings);
 
 // Initialize Firebase
 var db = firebase.firestore();
 //Add
 
 function storeData(){
-db.collection("employee").doc(document.querySelector("#employeeName").value).set({
-    name: document.querySelector("#employeeName").value,
-    Phone: document.querySelector("#employeePhone").value,
-    mail: document.querySelector("#employeeMail").value
-})
-.then(function() {
-    console.log("Erfolgreich hinzugefügt!");
-})
-.catch(function(error) {
-    console.error("Error writing document: ", error);
-});
+  db.collection("employee").doc(document.querySelector("#employeeName").value).set({
+      name: document.querySelector("#employeeName").value,
+      Phone: document.querySelector("#employeePhone").value,
+      mail: document.querySelector("#employeeMail").value
+  })
+  .then(function() {
+      console.log("Erfolgreich hinzugefügt!");
+  })
+  .catch(function(error) {
+      console.error("Error writing document: ", error);
+  });
 
 //show alert with timeout and reset
-document.querySelector(".alert").style.display ="block";
-setTimeout(function () {
-    document.querySelector(".alert").style.display ="none";
-},3000);
-document.getElementById("employee").reset();
+  document.querySelector(".alert").style.display ="block";
+  setTimeout(function () {
+      document.querySelector(".alert").style.display ="none";
+  },3000);
+  document.getElementById("employee").reset();
 }
